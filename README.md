649067be15fe736f11115ac86e86f65085d638ed
Samadi van Koten <samadi@vktec.org.uk>
2021-03-27 22:57:23 +0000

    Cause entity creation to load chunks
    This generalises the old dropper/dispenser loading and dimension change loading, as well
    as allowing some new things like movable entity loaders.

--- README.md
+++ README.md
@@ -9,5 +9,4 @@ A server-side mod that adds simple, flexible remote chunk loading mechanics to M
 For an overview of vanilla chunk loading mechanics in 1.14+, see [this page](https://gist.github.com/Drovolon/24bfaae00d57e7a8ca64b792e14fa7c6).
 
 - Block updates crossing chunk borders load the destination chunk as `TICKING` (lazy) for 1 gametick
-- Dispensing an item into the world loads the chunk the dropper or dispenser is in as `ENTITY_TICKING` for 1 gametick
-- An entity changing dimension loads the destination chunk as `TICKING` (lazy) for 1 gametick (nether portals will load for 299gt as normal)
+- Creating an entity in the world (including entities changing dimensions) will load the chunk it is created in as `ENTITY_TICKING` for 1 gametick
--- src/main/java/vktec/chonk/mixin/ServerWorldMixin.java
+++ src/main/java/vktec/chonk/mixin/ServerWorldMixin.java
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vktec.chonk.Chonk;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin {
-	@Inject(method = "onDimensionChanged", at = @At("HEAD"))
-	private void loadChunkOnDimensionChange(Entity entity, CallbackInfo ci) {
-		Chonk.loadTicking((ServerWorld)(Object)this, new ChunkPos(entity.getBlockPos()));
+	@Inject(method = "addEntity", at = @At("HEAD"))
+	private void loadChunkOnEntityCreation(Entity entity, CallbackInfoReturnable ci) {
+		Chonk.loadEntityTicking((ServerWorld)(Object)this, new ChunkPos(entity.getBlockPos()));
 	}
 }
